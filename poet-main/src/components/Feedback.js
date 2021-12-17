f7258bb308028ab72ea15ea5d9eb23a6b9fd15ab
Althis <mendesdej@corning.com>
2021-12-17 19:27:58 +0100

    final changes, fingers crossed

--- poet-main/src/components/Feedback.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class Feedback extends Component{
-    render(){
-        return (
-            <h2>Feedback box will return in Avengers 4</h2>
-        );
-    }
-}
